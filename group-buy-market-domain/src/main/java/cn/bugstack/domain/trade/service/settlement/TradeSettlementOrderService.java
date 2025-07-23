2d456b4feda01b574f1177d885572768b170988e
CorgiBoyG <rongjieguo2001@163.com>
2025-07-23 17:38:46 +0800

    fix: notifyTask.getNotifyCount() > 4 结算回调失败判断 2025年7月23日

--- group-buy-market-domain/src/main/java/cn/bugstack/domain/trade/service/settlement/TradeSettlementOrderService.java
+++ group-buy-market-domain/src/main/java/cn/bugstack/domain/trade/service/settlement/TradeSettlementOrderService.java
@@ -134,7 +134,7 @@ public class TradeSettlementOrderService implements ITradeSettlementOrderService
                     successCount += 1;
                 }
             } else if (NotifyTaskHTTPEnumVO.ERROR.getCode().equals(response)) { //失败
-                if (notifyTask.getNotifyCount() < 5) {
+                if (notifyTask.getNotifyCount() > 4) {
                     int updateCount = repository.updateNotifyTaskStatusError(notifyTask.getTeamId());
                     if (1 == updateCount) {
                         errorCount += 1;
